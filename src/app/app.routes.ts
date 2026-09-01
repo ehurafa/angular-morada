d0b6b0ffdccea290ba4c6335cb68daff053c7ec6
Rafael Gomes <rafarfl2010@gmail.com>
2026-09-01 17:53:03 -0300

    feat(properties): register property details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'imoveis/:id',
+    loadComponent: () =>
+      import('./features/properties/presentation/pages/property-details-page/property-details-page').then(
+        ({ PropertyDetailsPage }) => PropertyDetailsPage,
+      ),
+  },
+  {
     path: '',
     loadComponent: () =>
       import('./features/properties/presentation/pages/property-search-page/property-search-page').then(
